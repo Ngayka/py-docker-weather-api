28440f5b0ffcf507f095da78e2bddcd4cf7eb7bd
Ngayka <ngayka@gmail.com>
2025-06-29 10:28:27 +0300

    Implement weather service for Paris and publish Dockerized version: - Added a Python script to fetch current weather in Paris using WeatherAPI - API key is read securely from environment variables - Introduced .env support with python-dotenv (for local use) - Created Dockerfile with a minimal Python 3.11-slim image - Added .dockerignore to reduce image size and exclude secrets - Defined requirements.txt with requests and python-dotenv - Successfully built and pushed image to Docker Hub: ngayka/weather-service

--- app/main.py
+++ app/main.py
@@ -1,7 +1,28 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.environ.get("API_KEY")
+    if not api_key:
+        print("API_Key is not in environment")
+        return
+    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q=Paris&aqi=no"
+    res = requests.get(url)
+
+    if res.status_code != 200:
+        print(f"Error: {res.status_code}")
+        print(f"Response: {res.text}")
+    data = res.json()
+    location = data["location"]["name"]
+    temperature = data["current"]["temp_c"]
+    condition = data["current"]["condition"]["text"]
+    humidity = data["current"]["humidity"]
+    return f"Weather in {location}: {temperature}C, {humidity}% humidity, condition: {condition}"
 
 
 if __name__ == "__main__":
-    get_weather()
+    print(get_weather())
